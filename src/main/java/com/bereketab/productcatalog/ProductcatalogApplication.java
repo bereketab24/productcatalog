3ef97f4012cf74fcc61f7fe1531a030090ff6fef
bereketab24 <bereketab1994@gmail.com>
2026-03-14 22:44:39 +0100

    chore: initial spring boot project setup with maven

--- src/main/java/com/bereketab/productcatalog/ProductcatalogApplication.java
+++ src/main/java/com/bereketab/productcatalog/ProductcatalogApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductcatalogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductcatalogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductcatalogApplication.class, args);
+    }
 
 }
